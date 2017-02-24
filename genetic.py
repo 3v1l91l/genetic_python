fa76cc1d0f951824b813e6458bd93a28b58d3507
Misha Zhukov <3v1l91l@gmail.com>
2017-02-24 15:18:31 +0200

    Fix get_fitness

--- genetic.py
+++ genetic.py
@@ -1,5 +1,5 @@
 def get_fitness(guess, target):
-    return sum(1 for expected, actual in zip(guess.Genes, target.Genes)
+    return sum(1 for expected, actual in zip(guess, target)
                if expected == actual)
 
 class Chromosome:
