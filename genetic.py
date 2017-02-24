d77278b0be498af43066680f1622c9a81307a24d
Misha Zhukov <3v1l91l@gmail.com>
2017-02-24 15:17:31 +0200

    Add Chromosome class

--- genetic.py
+++ genetic.py
@@ -1,3 +1,8 @@
 def get_fitness(guess, target):
-    return sum(1 for expected, actual in zip(guess, target)
+    return sum(1 for expected, actual in zip(guess.Genes, target.Genes)
                if expected == actual)
+
+class Chromosome:
+    def __init__(self, genes, fitness):
+        self.Genes = genes
+        self.Fitness = fitness
