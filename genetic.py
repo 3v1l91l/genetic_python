5d1437dbe61c39e45b0054ffc30fc58ca985d7d2
Misha Zhukov <3v1l91l@gmail.com>
2017-02-24 15:41:24 +0200

    Add chromosome generation

--- genetic.py
+++ genetic.py
@@ -1,8 +1,21 @@
+import random
+
 def get_fitness(guess, target):
     return sum(1 for expected, actual in zip(guess, target)
                if expected == actual)
+
+def _generate_chromosome(gene_set, seq_length, get_fitness):
+    gene_list = []
+    while len(gene_list) < seq_length:
+        sample_length = min(len(gene_set), seq_length - (len(gene_list)))
+        gene_list.extend(random.sample(gene_set, sample_length))
+
+    genes = ''.join(gene_list)
+    fitness = get_fitness(genes)
+    return Chromosome(genes, fitness)
 
 class Chromosome:
     def __init__(self, genes, fitness):
         self.Genes = genes
         self.Fitness = fitness
+
--- geneticTests.py
+++ geneticTests.py
@@ -2,6 +2,8 @@ import unittest
 import genetic
 
 class GeneticTests(unittest.TestCase):
+    GeneSet = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!.,"
+
     def test_get_fitness_empty_guess(self):
         expected_fitness = 0
         actual_fitness = genetic.get_fitness(guess='', target='Test')
@@ -25,6 +27,17 @@ class GeneticTests(unittest.TestCase):
         actual_fitness = genetic.get_fitness(guess='12st', target='Test')
 
         self.assertEqual(actual_fitness, expected_fitness)
+
+    def test_generate_chromosome(self):
+        target = 'Test'
+
+        def fnGetFitness(genes):
+            return genetic.get_fitness(genes, target)
+
+        actual_chromosome = genetic._generate_chromosome(self.GeneSet, len(target), fnGetFitness)
+
+        self.assertEqual(len(actual_chromosome.Genes), len(target))
+        self.assertIsNotNone(actual_chromosome.Fitness)
 
 if __name__ == '__main__':
     unittest.main()
